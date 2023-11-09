79a6325e4aaed3fc1e511fd94b2ec93dd583f347
RomanSavitskiy <roma.savitskiy@inbox.ru>
2023-11-09 18:05:59 +0300

    Initial commit

---
+++ src/test/java/Task14Test.java
@@ -0,0 +1,27 @@
+import lab1.task14.Book;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Task14Test {
+    Book book1;
+    Book book2;
+
+    @Before
+    public void init() {
+        book1 = new Book("20000 leagues under the water", "Jules Verne", 5);
+        book2 = new Book();
+    }
+
+    @Test
+    public void testCloneOnAllArgsConstructor() {
+        Book clone = book1.clone();
+        Assert.assertEquals(book1, clone);
+    }
+
+    @Test
+    public void testCloneOnNoArgsConstructor() {
+        Book clone = book2.clone();
+        Assert.assertEquals(book2, clone);
+    }
+}
